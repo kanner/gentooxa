87d64fd4f883f22f813a013a421903e7a4e8cede
Andrew Kanner <andrew.kanner@gmail.com>
2014-07-15 13:49:49 +0400

    installed basic software (/root/gentoo-packages.sh) and set some configuration

---
+++ root/gentoo-packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+emerge -uavDN gentoolkit wgetpaste app-crypt/gnupg ppp pptpd dhcpcd wpa_supplicant wireless-tools gparted parted xfsprogs reiserfsprogs jfsutils genkernel pcmciautils syslog-ng mlocate ccache strace qemu transmission qutim app-misc/mc vim htop sudo xorg-server xorg-x11 nvidia-drivers corefonts fluxbox eterm xterm xtrlock gqview xclock gkrellm xdm xlockmore gimp commonbox-styles commonbox-styles-extra fluxbox-styles-fluxmod gentoo-artwork fluxbox-syntax idesk gentoo xv xzgv emacs rox gvim dropbox conky lynx madplay mpg123 chromium firefox thunderbird filezilla libreoffice apache mysql php phpmyadmin git openoffice-bin sys-firmware/iwl2030-ucode terminus-font mesa-progs alsa-utils eix adobe-flash pcsc-lite gdb cowsay media-video/vlc bc licq xfe tilda pptpclient evince tightvnc dnsmasq bridge-utils virtualbox acpitool mcrypt clamav p7zip rpm bootchart2 wireshark tkdiff traceroute ntfs3g layman wine laptop-mode-tools lshw dosbox minicom setserial putty imagemagick metasploit subversion gpart testdisk xxkb lsof iotop unetbootin irssi rar telnet-bsd telnetd enlightenment tor privoxy convmv ddd rogue v4l-utils volumeicon tint2 x11-misc/nitrogen orage uae e-uae dev-util/bunny hercules vsftpd suite3270 sshpass cscope ruby rubygems rpm dpkg xbacklight ufw bash-completion sleuthkit autopsy foremost valgrind hibernate-script gource samba rdesktop fakeroot alien freemind xmind texlive texlive-latexextra tcsh deb2targz skype paxtest cntlm =sys-devel/gcc-4.6.3 =sys-devel/gcc-4.5.4 =sys-devel/gcc-4.4.7 =sys-devel/gcc-4.3.6-r1 =sys-devel/gcc-4.1.2 =sys-devel/gcc-3.4.6-r2 zenity cppcheck include-what-you-use splint winetricks adom doxygen virt-manager mirrorselect
+
+#gem19 install fpm
+#gem20 install fpm
+
+#layman -a steam
+#layman -a sublime-text
+#emerge -uavDN steam-meta sublime-text
+
+### not installed packages
+#bumblebee
+#bbswitch
+
+### replaced packages
+#eclipse-sdk [emacs]
+#gedit [emacs]
+#nm-applet [-networkmanager USE]
+#vixie-cron [cronie]
+#gdm [xdm]
+#roxterm [tilda]
+#gnome-terminal [tilda]
+#nautilus [xfe]
+#evolution [thunderbird]
+#kile
